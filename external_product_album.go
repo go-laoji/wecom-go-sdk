c907b6b54c49b7c904d736148280c3d3b26afbfd
go-laoji <us.hipeace86@gmail.com>
2021-12-18 22:06:03 +0800

    管理商品图册

---
+++ external_product_album.go
@@ -0,0 +1,139 @@
+package wework
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/go-laoji/wework/internal"
+)
+
+type Product struct {
+	Description string `json:"description" validate:"required,max=300"`
+	Price       int    `json:"price" validate:"required,max=5000000"`
+	ProductSn   string `json:"product_sn" validate:"omitempty,max=128,"`
+	Attachments []struct {
+		Type  string `json:"type"`
+		Image struct {
+			MediaID string `json:"media_id"`
+		} `json:"image"`
+	} `json:"attachments" validate:"required"`
+}
+type AddProductAlbumResponse struct {
+	internal.BizResponse
+	ProductId string `json:"product_id"`
+}
+
+// AddProductAlbum 创建商品图册
+// https://open.work.weixin.qq.com/api/doc/90001/90143/95131#%E5%88%9B%E5%BB%BA%E5%95%86%E5%93%81%E5%9B%BE%E5%86%8C
+func (ww weWork) AddProductAlbum(corpId uint, product Product) (resp AddProductAlbumResponse) {
+	if ok := validate.Struct(product); ok != nil {
+		resp.ErrCode = 500
+		resp.ErrorMsg = ok.Error()
+	}
+	queryParams := ww.buildCorpQueryToken(corpId)
+	body, err := internal.HttpPost(fmt.Sprintf("/cgi-bin/externalcontact/add_product_album?%s", queryParams.Encode()), product)
+	if err != nil {
+		resp.ErrCode = 500
+		resp.ErrorMsg = err.Error()
+	} else {
+		json.Unmarshal(body, &resp)
+	}
+	return
+}
+
+type GetProductAlbumResponse struct {
+	internal.BizResponse
+	Product struct {
+		Product
+		ProductId  string `json:"product_id"`
+		CreateTime int    `json:"create_time"`
+	} `json:"product"`
+}
+
+// GetProductAlbum 获取商品图册
+// https://open.work.weixin.qq.com/api/doc/90001/90143/95131#%E8%8E%B7%E5%8F%96%E5%95%86%E5%93%81%E5%9B%BE%E5%86%8C
+func (ww weWork) GetProductAlbum(corpId uint, productId string) (resp GetProductAlbumResponse) {
+	queryParams := ww.buildCorpQueryToken(corpId)
+	h := H{}
+	h["product_id"] = productId
+	body, err := internal.HttpPost(fmt.Sprintf("/cgi-bin/externalcontact/get_product_album?%s", queryParams.Encode()), h)
+	if err != nil {
+		resp.ErrCode = 500
+		resp.ErrorMsg = err.Error()
+	} else {
+		json.Unmarshal(body, &resp)
+	}
+	return
+}
+
+type GetProductAlbumListResponse struct {
+	internal.BizResponse
+	NextCursor  string `json:"next_cursor"`
+	ProductList []struct {
+		ProductId string `json:"product_id"`
+		Product
+	} `json:"product_list"`
+}
+
+// GetProductAlbumList 获取商品图册列表
+// https://open.work.weixin.qq.com/api/doc/90001/90143/95131#%E8%8E%B7%E5%8F%96%E5%95%86%E5%93%81%E5%9B%BE%E5%86%8C%E5%88%97%E8%A1%A8
+func (ww weWork) GetProductAlbumList(corpId uint, limit int, cursor string) (resp GetProductAlbumListResponse) {
+	queryParams := ww.buildCorpQueryToken(corpId)
+	h := H{}
+	h["limit"] = limit
+	h["cursor"] = cursor
+	body, err := internal.HttpPost(fmt.Sprintf("/cgi-bin/externalcontact/get_product_album?%s", queryParams.Encode()), h)
+	if err != nil {
+		resp.ErrCode = 500
+		resp.ErrorMsg = err.Error()
+	} else {
+		json.Unmarshal(body, &resp)
+	}
+	return
+}
+
+type ProductUpdateRequest struct {
+	ProductId   string `json:"product_id" validate:"required"`
+	Description string `json:"description,omitempty" validate:"required,max=300"`
+	Price       int    `json:"price,omitempty" validate:"required,max=5000000"`
+	ProductSn   string `json:"product_sn,omitempty" validate:"omitempty,max=128,"`
+	Attachments []struct {
+		Type  string `json:"type"`
+		Image struct {
+			MediaID string `json:"media_id"`
+		} `json:"image"`
+	} `json:"attachments,omitempty" validate:"required"`
+}
+
+// UpdateProductAlbum 编辑商品图册
+// https://open.work.weixin.qq.com/api/doc/90001/90143/95131#%E7%BC%96%E8%BE%91%E5%95%86%E5%93%81%E5%9B%BE%E5%86%8C
+func (ww weWork) UpdateProductAlbum(corpId uint, request ProductUpdateRequest) (resp internal.BizResponse) {
+	if ok := validate.Struct(request); ok != nil {
+		resp.ErrCode = 500
+		resp.ErrorMsg = ok.Error()
+	}
+	queryParams := ww.buildCorpQueryToken(corpId)
+	body, err := internal.HttpPost(fmt.Sprintf("/cgi-bin/externalcontact/update_product_album?%s", queryParams.Encode()), request)
+	if err != nil {
+		resp.ErrCode = 500
+		resp.ErrorMsg = err.Error()
+	} else {
+		json.Unmarshal(body, &resp)
+	}
+	return
+}
+
+// DeleteProductAlbum 删除商品图册
+// https://open.work.weixin.qq.com/api/doc/90001/90143/95131#%E5%88%A0%E9%99%A4%E5%95%86%E5%93%81%E5%9B%BE%E5%86%8C
+func (ww weWork) DeleteProductAlbum(corpId uint, productId string) (resp internal.BizResponse) {
+	queryParams := ww.buildCorpQueryToken(corpId)
+	h := H{}
+	h["product_id"] = productId
+	body, err := internal.HttpPost(fmt.Sprintf("/cgi-bin/externalcontact/delete_product_album?%s", queryParams.Encode()), h)
+	if err != nil {
+		resp.ErrCode = 500
+		resp.ErrorMsg = err.Error()
+	} else {
+		json.Unmarshal(body, &resp)
+	}
+	return
+}
